a63cc7cf02d718833b80517bfdcf42ae54dba552
SongMinJun <dkaskgkdua@gmail.com>
2020-10-09 19:59:29 +0900

    다국어 처리를 Internationalization 구현해보기

--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -3,10 +3,18 @@ package com.example.restfulwebservice.helloworld;
 import com.example.restfulwebservice.user.User;
 import com.example.restfulwebservice.user.UserDaoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
     // GET
     // /hello-world (endpoint)
     // @RequestMapping(method=RequestMethod.GET, path ="/hello-world")
@@ -25,4 +33,9 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name="Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message",null, locale);
+    }
 }
