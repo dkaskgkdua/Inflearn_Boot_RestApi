5cc577cec9c55820fc0aec675672592a3e0fde52
SongMinJun <dkaskgkdua@gmail.com>
2020-10-09 23:41:55 +0900

    - response 데이터 제어를 위한 필터링(주민번호나 비밀번호를 제외하기 위한) - xml 방식으로 응답하기(라이브러리)

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,5 +1,7 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +13,7 @@ import java.util.Date;
 @Data
 @Builder
 @AllArgsConstructor
+@JsonIgnoreProperties(value ={"password"})
 public class User {
     private Integer id;
     @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
@@ -18,4 +21,9 @@ public class User {
     // 과거 데이터만 올 수가 있다(시간)
     @Past
     private Date joinDate;
+    // response 응답 시 제외
+//    @JsonIgnore
+    private String password;
+//    @JsonIgnore
+    private String ssn;
 }
