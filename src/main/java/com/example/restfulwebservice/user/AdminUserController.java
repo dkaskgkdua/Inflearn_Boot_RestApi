1001b142b0ff2ab79b7e3ad963216e9141d29387
SongMinJun <dkaskgkdua@gmail.com>
2020-10-11 21:26:47 +0900

    Param과 Header를 이용한 REST API Version관리 - params - headers - produces

--- src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
@@ -22,7 +23,7 @@ public class AdminUserController {
     private final UserDaoService service;
 
     @GetMapping("/users")
-    public MappingJacksonValue retrieveAllUsers() {
+    public MappingJacksonValue retrieveAllUsers(HttpServletResponse response) {
         List<User> users = service.findAll();
 
         SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
@@ -33,11 +34,17 @@ public class AdminUserController {
 
         MappingJacksonValue mapping = new MappingJacksonValue(users);
         mapping.setFilters(filters);
-
         return mapping;
     }
-
-    @GetMapping("/v1/users/{id}")
+    //1. 버전 없는 경우
+    //@GetMapping("/v1/users/{id}")
+    //2. url 파람으로 버전관리
+    //@GetMapping(value = "/users/{id}/", params = "version=1")
+    // 3번부터는 일반브라우저에서 실행 불가
+    //3. header 정보 추가해서 버전관리
+    //@GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+    //4. MINE타입 = application, 써드타입밴더 = vnd.company, 버전 이름 = appv1, 타입 = json
+    @GetMapping(value = "/users/{id}", produces="application/vnd.company.appv1+json")
     public MappingJacksonValue retrieveUser1(@PathVariable int id) {
         User user = service.findOne(id);
 
@@ -54,7 +61,10 @@ public class AdminUserController {
         mapping.setFilters(filters);
         return mapping;
     }
-    @GetMapping("/v2/users/{id}")
+    //@GetMapping("/v2/users/{id}")
+    //@GetMapping(value = "/users/{id}/", params = "version=2")
+    //@GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/users/{id}", produces="application/vnd.company.appv2+json")
     public MappingJacksonValue retrieveUser2(@PathVariable int id) {
         User user = service.findOne(id);
 
