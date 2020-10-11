713a71c4668b51c773dbc29a601ecc71ce89e2e5
SongMinJun <dkaskgkdua@gmail.com>
2020-10-11 18:11:34 +0900

    프로그래밍으로 제어하는 Filtering 방법 - 개별 사용자 - 전체 사용자

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,5 +1,6 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
@@ -13,7 +14,9 @@ import java.util.Date;
 @Data
 @Builder
 @AllArgsConstructor
-@JsonIgnoreProperties(value ={"password"})
+// json 반환값에 password를 제외한다.
+//@JsonIgnoreProperties(value ={"password"})
+@JsonFilter("UserInfo")
 public class User {
     private Integer id;
     @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
